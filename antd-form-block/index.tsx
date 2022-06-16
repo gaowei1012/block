42a98dec389b761fc8a3acf905b4f673d5312b5c
gaowei1012 <gyb2020018@163.com>
2022-06-16 10:38:32 +0800

    初始化项目配置

---
+++ antd-form-block/index.tsx
@@ -0,0 +1,13 @@
+/*
+ * @Author: mingwei
+ * @Date: 2022-06-16 10:31:38
+ * @LastEditors: mingwei
+ * @LastEditTime: 2022-06-16 10:31:39
+ * @FilePath: /antd-block/antd-form-block/index.tsx
+ * @Description:
+ */
+import React from "react";
+
+export const AntdFormBlock = () => {
+  return <>form</>;
+};
